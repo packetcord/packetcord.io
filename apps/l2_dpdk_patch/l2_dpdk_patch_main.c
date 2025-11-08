5e7634ec96f8026ec45acf0a9ab20c2fdb5894b0
Varban Metodiev <varban.metodiev@gmail.com>
2025-11-08 19:46:38 +0200

    Example app l2_dpdk_patch - in progress.

--- apps/l2_dpdk_patch/l2_dpdk_patch_main.c
+++ apps/l2_dpdk_patch/l2_dpdk_patch_main.c
@@ -6,16 +6,18 @@
 #include <cord_type.h>
 #include <rte_eal.h>
 #include <rte_errno.h>
+#include <rte_ethdev.h>
 
-#define MTU_SIZE 1500
-#define ETHERNET_HEADER_SIZE 14
-#define DOT1Q_TAG_SIZE 4
+#define RX_RING_SIZE 1024
+#define TX_RING_SIZE 1024
 
-#define BUFFER_SIZE (MTU_SIZE + ETHERNET_HEADER_SIZE)
+#define NUM_MBUFS 8191
+#define MBUF_CACHE_SIZE 250
+#define BURST_SIZE 32
 
 static struct
 {
-
+    struct rte_mempool* cord_pktmbuf_mpool;
 } cord_app_context;
 
 static void cord_app_setup(void)
@@ -25,10 +27,11 @@ static void cord_app_setup(void)
 
 static void cord_app_cleanup(void)
 {
-    if (rte_eal_cleanup() == 0)
-        CORD_LOG("[CordApp] DPDK EAL cleanup.\n");
-    else
-        CORD_ERROR("[CordApp] DPDK EAL cleanup failed!\n");
+    CORD_LOG("[CordApp] DPDK Packet Mbuf and Mempool cleanup.\n");
+    cord_pktmbuf_mpool_free(cord_app_context.cord_pktmbuf_mpool);
+
+    CORD_LOG("[CordApp] DPDK EAL cleanup.\n");
+    cord_eal_cleanup();
 }
 
 static void cord_app_sigint_callback(int sig)
@@ -40,19 +43,25 @@ static void cord_app_sigint_callback(int sig)
 
 int main(void)
 {
-    char *cord_eal_argv[] = {
-        "l2_dpdk_patch",      // Program name
-        "-l", "0-1",          // Logical cores to use (0-1)
-        "--proc-type=auto",   // Process type (auto-detect primary/secondary)
-        "--log-level=8",      // Log level (8 = debug)
-    };
+    signal(SIGINT, cord_app_sigint_callback);
 
-    // --no-pci --vdev net_af_xdp0,iface=veth1 --vdev net_af_xdp1,iface=veth2 --log-level=pmd.net.af_xdp:debug
+    char *cord_eal_argv[] = {
+        "l2_dpdk_patch_app",                // Program name (argv[0])
+        "-l", "0-1",                        // Logical cores to use (0-1)
+        "--proc-type=auto",                 // Process type (auto-detect primary/secondary)
+        "--no-pci",                         // Do not probe for physical or virtual PCIe devices
+        "--vdev=net_af_xdp0,iface=veth1",   // Bind via AF_XDP PMD to veth1
+        "--vdev=net_af_xdp1,iface=veth2",   // Bind via AF_XDP PMD to veth2
+        "--log-level=8",                    // Log level (8 = debug)
+    };
 
     int cord_eal_argc = sizeof(cord_eal_argv) / sizeof(cord_eal_argv[0]);
     cord_eal_init(cord_eal_argc, cord_eal_argv);
 
-    signal(SIGINT, cord_app_sigint_callback);
+    uint16_t nb_ports = rte_eth_dev_count_avail();
+    CORD_LOG("[CordApp] Total DPDK ports: %u\n", nb_ports);
+
+    cord_app_context.cord_pktmbuf_mpool = cord_pktmbuf_mpool_alloc("MBUF_POOL", NUM_MBUFS * nb_ports, MBUF_CACHE_SIZE);
 
     while (1)
     {
